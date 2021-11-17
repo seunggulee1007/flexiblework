9b8251db3a80170669c3b027294d58d93f1599a2
seunggu lee <leesg107@tpsg.co.kr>
2021-11-17 19:28:58 +0900

    feat : 유연근무 enum클래스 화면에서 쓰일 코드로 변환 작업

--- src/main/java/com/secommon/separtners/modules/flexiblework/enums/FlexibleWorkType.java
+++ src/main/java/com/secommon/separtners/modules/flexiblework/enums/FlexibleWorkType.java
@@ -1,10 +1,36 @@
 package com.secommon.separtners.modules.flexiblework.enums;
 
-public enum FlexibleWorkType {
+import com.secommon.separtners.modules.common.EnumMapperType;
 
-    WEEK_52,
-    ELASTIC,
-    CHOICE,
-    HOME,
+public enum FlexibleWorkType implements EnumMapperType {
+
+    WEEK_52("주 52시간", false),
+    ELASTIC("탄력근무제", false),
+    CHOICE("선택적근무제", false),
+    HOME("재택 근무제", false),
+    TIME("시차출퇴근제", false),
+    ;
+    String codeName;
+    boolean defaultValue;
+
+    FlexibleWorkType(String codeName, boolean defaultValue) {
+        this.codeName = codeName;
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public String getCode () {
+        return name();
+    }
+
+    @Override
+    public String getTitle () {
+        return codeName;
+    }
+
+    @Override
+    public boolean isDefault () {
+        return defaultValue;
+    }
 
 }
--- src/main/java/com/secommon/separtners/modules/flexiblework/enums/WorkDayOfWeek.java
+++ src/main/java/com/secommon/separtners/modules/flexiblework/enums/WorkDayOfWeek.java
@@ -1,11 +1,37 @@
 package com.secommon.separtners.modules.flexiblework.enums;
 
-public enum WorkDayOfWeek {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY,
+import com.secommon.separtners.modules.common.EnumMapperType;
+
+public enum WorkDayOfWeek implements EnumMapperType {
+
+    MONDAY("월요일", false),
+    TUESDAY("화요일", false),
+    WEDNESDAY("수요일", false),
+    THURSDAY("목요일", false),
+    FRIDAY("금요일", false),
+    SATURDAY("토요일", false),
+    SUNDAY("일요일", false),
+    ;
+    String codeName;
+    boolean defaultValue;
+
+    WorkDayOfWeek(String codeName, boolean defaultValue) {
+        this.codeName = codeName;
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public String getCode () {
+        return name();
+    }
+
+    @Override
+    public String getTitle () {
+        return codeName;
+    }
+
+    @Override
+    public boolean isDefault () {
+        return defaultValue;
+    }
 }
