a977c9da243a0c0eb9d3acb9d65769e67f88f3f8
seunggu lee <leesg107@tpsg.co.kr>
2021-11-17 19:33:03 +0900

    style : 주석 및 공백 제거

--- src/main/java/com/secommon/separtners/infra/converter/StringEncryptConverter.java
+++ src/main/java/com/secommon/separtners/infra/converter/StringEncryptConverter.java
@@ -14,14 +14,7 @@ import java.util.Optional;
 public class StringEncryptConverter implements AttributeConverter<String, String> {
 
     private final StringEncryptor stringEncryptor;
-
-//    @Autowired
-//    @Qualifier("jasyptStringEncryptor")
-//    public void setStringEncryptor(StringEncryptor encryptor) {
-//        this.stringEncryptor = encryptor;
-//    }
-
-
+    
     @Override
     public String convertToDatabaseColumn ( String attribute ) {
         return Optional.ofNullable(attribute)
